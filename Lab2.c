0f802e50852579926e65c4a83ab4ba145d3fa888
Krutarth Patel <krutarth2000@gmail.com>
2019-05-12 00:42:00 -0400

    Add files via upload

---
+++ Lab2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char** argv)
+{
+    double originalCost, interest, numMonths, payment;
+
+    printf("Enter the purchase price of the item: "); 
+    scanf("%lf", &originalCost);
+
+    printf("Enter the interest rate per year as a percentage: "); 
+    scanf("%lf", &interest);
+
+    printf("Enter the number of months for repayment: "); 
+    scanf("%lf", &numMonths); 
+
+    interest = (interest / 100) / 12; // calculates interest rate for months
+
+    payment = originalCost * interest / (1 - (pow(1 + interest, -1 * (numMonths)))); // calculates the monthly payment amount
+
+    printf("The monthly payment is: %.2lf\n", payment); // displays the payment amount
+    printf("The total cost of the purchase is: %.2lf\n", payment * numMonths); 
+    return 0;
+}
